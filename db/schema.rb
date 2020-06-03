c16ddbecc45fe32847bf4c8d1f835fd592104b0c
Wellington <wjas@globo.com>
2020-06-03 00:54:16 -0300

    Create migrations to articles table

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_06_03_034721) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
