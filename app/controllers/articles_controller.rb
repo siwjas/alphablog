82e3679f6a15ca201be6a994b27449217427a6c0
Wellington <wjas@globo.com>
2020-06-06 21:45:25 -0300

    Created new article page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,19 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      flash[:notice] = 'Article was saved successfully!'
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
 end
