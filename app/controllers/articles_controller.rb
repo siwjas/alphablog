d44eb20e961acb4164e19f388bd226bd63a13ae3
Wellington <wjas@globo.com>
2020-06-07 14:43:00 -0300

    refactored code to apply the DRY concetp

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,11 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   def index
     @articles = Article.all
   end
   
   def show
-    @article = Article.find(params[:id])
   end
 
 
@@ -14,11 +14,10 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     if @article.save
       flash[:notice] = 'Article was saved successfully!'
       redirect_to @article
@@ -28,9 +27,7 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    #byebug
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(article_params)
       flash[:notice] = 'Article updated successfully!'
       redirect_to @article
     else
@@ -39,9 +36,18 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
   end
 
 end
