559e4889c6d384a263fb979c436e714f38ddf198
Wellington <wjas@globo.com>
2020-06-08 01:07:14 -0300

    Stylized articles listing page

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
   validates_presence_of :title, :description
   validates :title, length: { in: 6..128 }
-  validates :description, length: { in: 10..256 }
+  validates :description, length: { in: 10..1000 }
 end
