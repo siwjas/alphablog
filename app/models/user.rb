339f7e0e981b450dc0cc767912136e000e3774e6
Wellington <wjas@globo.com>
2020-06-10 03:14:33 -0300

    add password digest to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,17 @@
 class User < ApplicationRecord
-  bofore_save { self.email = email.downcase }
+  before_save { self.email = email.downcase }
   has_many :articles
   validates :username,  presence: true, 
                         uniqueness: { case_sensitive: false }, 
                         length: { in: 3..25 }
+  
+  validates :password,  presence: true, 
+                        length { minimum: 6 }
 
   VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :email, presence: true, 
                     length: { maximum: 105 },
                     format: { with: VALID_EMAIL_REGEX }
+  
+  has_secure_password
 end 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_09_032740) do
+ActiveRecord::Schema.define(version: 2020_06_10_055231) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_06_09_032740) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
