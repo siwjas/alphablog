8fedd09cd80715afb7fa3842b92aa6b30f8b7c9f
Wellington <wjas@globo.com>
2020-06-13 12:21:57 -0300

    user profile page refined

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update, :show]
 
+  def index
+    @users = User.all
+  end
+  
   def show
     @articles = @user.articles
   end
