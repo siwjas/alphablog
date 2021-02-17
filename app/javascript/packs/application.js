159fd0240a8ffda7915ad76d07ec8ef3ded10100
Wellington <wjas@globo.com>
2021-02-16 23:58:32 -0300

    Update navigation

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 import "bootstrap"
+import "bootstrap-icons/font/bootstrap-icons.css"
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
