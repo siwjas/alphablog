304cd793874da0a51a8c6621e915bef3a32410d9
Wellington <wjas@globo.com>
2021-02-16 22:08:05 -0300

    Completed categories index action with pagination

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,7 +6,7 @@ class CategoriesController < ApplicationController
   end
 
   def index
-    @categories = Category.paginate(page: params[:page], per_page: 5)    
+    @categories = Category.paginate(page: params[:page], per_page: 5)
   end
 
   def create
