22941474b2ff351d4bb2c9b5b68850539285b269
Wellington <wjas@globo.com>
2021-02-19 14:39:35 -0300

    config bootstrap-icons on herroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,8 @@ Rails.application.configure do
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
-
+  config.assets.precompile += %w( '.woff', '.eot', '.svg', '.ttf' )
+  
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
